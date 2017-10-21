0412922b196f3e5d3ebd678d6f83270cca3864fc
DotheNald <32904520+DotheNald@users.noreply.github.com>
2017-10-21 17:21:06 +0200

    Add files via upload
    Ertes Interface & daraus generierter Test.

---
+++ ProjectInterface.java
@@ -0,0 +1,7 @@
+package se1_02;
+
+public interface ProjectInterface {
+	public boolean isWellSorted(String [] sequence);
+	
+
+}
---
+++ ProjectInterfaceTest.java
@@ -0,0 +1,19 @@
+package se1_02;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProjectInterfaceTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
