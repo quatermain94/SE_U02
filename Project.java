65d4cbb5417f7b02e16f8c9ae159c6085a4c2bfd
quatermain94 <32904513+quatermain94@users.noreply.github.com>
2017-10-21 19:39:11 +0200

    Version 2

--- Project.java
+++ Project.java
@@ -13,7 +13,32 @@ public class Project implements ProjectInterface {
 		if (sequence.length == 0){						//sequenz leer
 			return false;								//... ist auch doof
 		}
-		return true;
+		boolean erfuellt = true;
+		
+		fehler:											//bei fehler wird die schleife mitabgebrochen
+		for (int i = 0; i<bedingungen.length; i++){		//für alle Bedingungen
+			int m = 0;
+			while (bedingungen[i][0] != sequence[m]){	//suche den anfang der bed in der sequenz
+				m++;
+				if (m >= sequence.length){				//falls nicht vorhanden
+					erfuellt = false;					//ist die bed nicht erfüllt
+					break fehler;						//also kannste abbrechen
+				}
+			}
+			int n = m+1;
+			if (n >= sequence.length){					//sequenz zuende
+				erfuellt = false;						//ist die bed nicht erfüllt
+				break fehler;							//also kannste abbrechen
+			}
+			while (bedingungen[i][1] != sequence[n]){	//suche den 2. teil der bedingung in dert sequenz
+				n++;
+				if (n >= sequence.length){				//falls nicht vorhanden
+					erfuellt = false;					//ist die bed nicht erfüllt
+					break fehler;						//also kannste abbrechen
+				}
+			}
+		}
+		return erfuellt;
 	}
 
 }
